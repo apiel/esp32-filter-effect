361597396162f564086c372160f8f040b7d7b27b
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-02-26 18:18:39 +0100

    forward in to out

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,16 @@ void setup()
 {
   LOGLEVEL_AUDIOKIT = AudioKitInfo;
   Serial.begin(115200);
-  // open in write mode
-  // auto cfg = kit.defaultConfig(AudioOutput);
 
   auto cfg = kit.defaultConfig();
-  // cfg.adc_input = AUDIO_HAL_ADC_INPUT_ALL;
-  cfg.adc_input = AUDIO_HAL_ADC_INPUT_LINE1;
+  cfg.adc_input = AUDIO_HAL_ADC_INPUT_LINE2;
   cfg.sample_rate = AUDIO_HAL_16K_SAMPLES;
 
   kit.begin(cfg);
-
-  // 1000 hz
-  // wave.setFrequency(1000);
-  // wave.setSampleRate(cfg.sampleRate());
 }
 
 void loop()
 {
-  // size_t len = wave.read(buffer, BUFFER_SIZE);
   size_t len = kit.read(buffer, BUFFER_SIZE);
 
   // HERE will be some code to apply filters
